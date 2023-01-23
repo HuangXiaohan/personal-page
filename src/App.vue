8f07a956e7267c29963c8244622838bf423e1aba
Xiaohan HUANG (NM) <12794363+HuangXiaohan@users.noreply.github.com>
2023-01-23 23:19:19 +0100

    page header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+     <button type="button" class="btn btn-success">Success</button>
+     <button type="button" class="btn btn-info">Info</button>
+     <button type="button" class="btn btn-warning">Warning</button>
+  </div> -->
+  <ProfileHeader></ProfileHeader>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProfileHeader from './components/ProfileHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProfileHeader
+
   }
 }
 </script>
@@ -21,6 +28,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/ProfileHeader.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="background py-5">
+            <img class="circular--square me-4" src="../assets/picture.png" alt="Avatar">
+            <span class="profile-name">HUANG Xiaohan</span>
+
+    </div>
+    
+</template>
+
+<script>
+export default{
+    name: 'ProfileHeader'
+}
+</script>
+
+<style>
+.background {
+    background: url("../assets/bg-image.png") no-repeat;
+    background-size: 100%;
+}
+
+.circular--square { 
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    border-color:antiquewhite;
+    border-style: double;
+    border-width: 8px;
+ }
+
+.profile-name {
+    font-weight: 600;
+    font-size: 40px;
+    line-height:65px;
+    letter-spacing: 2px;
+    text-align: center;
+    text-transform: uppercase;
+}
+</style>
